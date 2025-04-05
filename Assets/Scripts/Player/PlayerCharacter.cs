54276a5a3c37928fe0de7baf2f3910243013efa8
Hansen <hansenwijaya2004@gmail.com>
2025-04-05 22:34:35 +0700

    player movements

--- Assets/Scripts/Player/PlayerCharacter.cs
+++ Assets/Scripts/Player/PlayerCharacter.cs
@@ -8,21 +8,47 @@ namespace Player
 {
     public class PlayerCharacter : MonoBehaviour
     {
-        [SerializeField]
-        private GroundMovementComponent movementComponent;
-        [SerializeField]
-        private HealthComponent healthComponent;
-        
+        [SerializeField] private GroundMovementComponent movementComponent;
+        [SerializeField] private HealthComponent healthComponent;
+        [SerializeField] private Animator animator;
+        [SerializeField] private SpriteRenderer spriteRenderer;
+
+        private Vector2 lastMoveDirection;
+
         private void Awake()
         {
             movementComponent ??= GetComponent<GroundMovementComponent>();
             healthComponent ??= GetComponent<HealthComponent>();
+            animator ??= GetComponent<Animator>();
+            spriteRenderer ??= GetComponent<SpriteRenderer>();
         }
 
         private void FixedUpdate()
         {
-            var input = new Vector2(Input.GetAxis("Horizontal"), Input.GetAxis("Vertical"));
+            Vector2 input = new Vector2(Input.GetAxisRaw("Horizontal"), Input.GetAxisRaw("Vertical"));
             movementComponent?.Move(input);
+
+            UpdateAnimator(input);
+        }
+
+        private void UpdateAnimator(Vector2 input)
+        {
+            bool isMoving = input != Vector2.zero;
+
+            if (isMoving)
+            {
+                lastMoveDirection = input.normalized;
+
+                // Flip sprite for left/right
+                if (Mathf.Abs(input.x) > 0.1f)
+                {
+                    spriteRenderer.flipX = input.x < 0;
+                }
+            }
+
+            animator.SetFloat("moveX", lastMoveDirection.x);
+            animator.SetFloat("moveY", lastMoveDirection.y);
+            animator.SetBool("isMoving", isMoving);
         }
     }
 }
