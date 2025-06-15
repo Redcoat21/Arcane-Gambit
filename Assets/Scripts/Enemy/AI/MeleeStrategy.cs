3bd829609ac1d8579f4ad4c1a99889b26031ecf0
Dahren Ericsson <qvbo7teu@registrat21.anonaddy.me>
2025-06-15 22:14:26 +0700

    feat: Added different strategy for enemy AI

---
+++ Assets/Scripts/Enemy/AI/MeleeStrategy.cs
@@ -0,0 +1,22 @@
+﻿using Components.Movements;
+using UnityEngine;
+using UnityEngine.AI;
+
+namespace Enemy.AI
+{
+    [CreateAssetMenu(fileName = "Melee Strategy", menuName = "Features/Strategy/Melee Strategy", order = 0)]
+    public class MeleeStrategy : EnemyStrategy
+    {
+        public override void Execute(Enemy enemy, Transform target)
+        {
+            var navMeshAgent = enemy.GetComponent<NavMeshAgent>();
+            if (navMeshAgent == null)
+            {
+                Debug.LogWarning("Navigation Mesh Agent is not set for " + enemy.name);
+                return;
+            }
+            // Move towards the target
+            navMeshAgent.Move(target.position);
+        }
+    }
+}
